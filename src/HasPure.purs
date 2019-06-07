6c5df76700fc67b37ebccb011afcc7127dbdd0f8
matthew.hilty <hilty.matthew@gmail.com>
2019-06-07 00:11:42 -0400

    snapshot (doesn't build)

--- src/HasPure.purs
+++ src/HasPure.purs
@@ -10,13 +10,19 @@ import Control.Restricted.HasConst (class HasConst, const)
 import Control.Restricted.HasEval (class HasEval, eval)
 import Control.Restricted.HasIdentity (class HasIdentity, identity)
 import Control.Restricted.HasMap (class HasMap, map, (<$>))
+import Control.Restricted.HasUnit (class HasUnit, unit)
 import Control.Restricted.ObjectOf (class ObjectOf)
+import Data.Unit (Unit)
+import Data.Unit (unit) as Unit
 
 class HasPure c f where
   pure :: forall v. ObjectOf c v => v -> f v
 
 type DictHasPure c f =
   { pure :: forall v. HasPure c f => ObjectOf c v => v -> f v }
+
+type DictHasUnit c u =
+  { unit :: HasUnit c u => ObjectOf c u => Unit -> u }
 
 liftA1
   :: forall c f v0 v1
@@ -29,23 +35,30 @@ liftA1
   -> c v0 v1
   -> f v0
   -> f v1
-liftA1 dictHasPure f x = dictHasPure.pure f <*> x
---   where
---   dictHasPure :: DictHasPure c f
---   dictHasPure = { pure }
+liftA1 { pure } f x = pure f <*> x
 
--- -- | Perform an applicative action when a condition is true.
--- when :: forall m. Applicative m => Boolean -> m Unit -> m Unit
--- when true m = m
--- when false _ = pure unit
---
+when
+  :: forall c m u
+   . HasPure c m
+  => HasUnit c u
+  => ObjectOf c u
+  => DictHasPure c m
+  -> Boolean
+  -> m u
+  -> m u
+when _ true m = m
+when { pure } false _ = pure (dictHasUnit.unit Unit.unit)
+  where
+  dictHasUnit :: DictHasUnit c u
+  dictHasUnit = { unit: \_ -> unit }
+
 -- -- | Perform an applicative action unless a condition is true.
 -- unless :: forall m. Applicative m => Boolean -> m Unit -> m Unit
 -- unless false m = m
 -- unless true _ = pure unit
---
--- instance hasPureUnrestricted
---   :: Unrestricted.Applicative f
---   => HasPure Function f
---   where
---   pure = Unrestricted.pure
+
+instance hasPureUnrestricted
+  :: Unrestricted.Applicative f
+  => HasPure Function f
+  where
+  pure = Unrestricted.pure
