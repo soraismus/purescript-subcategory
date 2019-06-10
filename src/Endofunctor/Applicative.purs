ca8b7adae396e9461367120eb7b36752a5c70e3c
matthew.hilty <hilty.matthew@gmail.com>
2019-06-10 09:16:28 -0400

    snapshot

--- src/Endofunctor/Applicative.purs
+++ src/Endofunctor/Applicative.purs
@@ -33,33 +33,6 @@ liftA1
   => Slackable c
   => c v0 v1
   -> c (f v0) (f v1)
-liftA1 f = restrictedFn
-  where
-  x0 :: f (c v0 v1)
-  x0 = slacken (pure' (Proxy3 :: Proxy3 c)) f
-
-  applyX0 :: c (f v0) (f v1)
-  applyX0 = apply x0
-
-  slackenApplyX0 :: f v0 -> f v1
-  slackenApplyX0 = slacken applyX0
-
-  fn :: f v0 -> f v1
-  fn = \fv0 -> slackenApplyX0 fv0
-
-  restrictedFn :: c (f v0) (f v1)
-  restrictedFn = restrict fn
-
---     slacken (pure' (Proxy3 :: Proxy3 c)) f :: f (c v0 v1)
---     apply (slacken (pure' (Proxy3 :: Proxy3 c)) f) :: c (f v0) (f v1)
---     slacken (apply (slacken (pure' (Proxy3 :: Proxy3 c)) f)) fv0 :: f v1
-
-
--- liftA1 f =
---   restrict \fx0 -> slacken applyFf fx0
---   where
---   ff :: f (c v0 v1)
--- --   ff = pure' (Proxy3 :: Proxy3 c)
---   ff = restrict (slacken (pure' (Proxy3 :: Proxy3 c)) f)
---   applyFf :: c (f v0) (f v1)
---   applyFf = apply ff
+liftA1 f =
+  restrict \fv0 ->
+    (slacken (apply (slacken (pure' (Proxy3 :: Proxy3 c)) f))) fv0
