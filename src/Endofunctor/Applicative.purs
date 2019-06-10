ff763e017c2c72123e749adebd613137824d534b
matthew.hilty <hilty.matthew@gmail.com>
2019-06-10 09:07:10 -0400

    snapshot

--- src/Endofunctor/Applicative.purs
+++ src/Endofunctor/Applicative.purs
@@ -27,11 +27,39 @@ liftA1
   => ObjectOf c (f v0)
   => ObjectOf c (f v1)
   => ObjectOf c (c v0 v1)
+  => ObjectOf c (f (c v0 v1))
   => ObjectOf c (c (f v0) (f v1))
   => Restrictable Function c
   => Slackable c
   => c v0 v1
   -> c (f v0) (f v1)
-liftA1 f =
-  restrict \fx0 ->
-    slacken (apply (pure' (Proxy3 :: Proxy3 c) f)) fx0
+liftA1 f = restrictedFn
+  where
+  x0 :: f (c v0 v1)
+  x0 = slacken (pure' (Proxy3 :: Proxy3 c)) f
+
+  applyX0 :: c (f v0) (f v1)
+  applyX0 = apply x0
+
+  slackenApplyX0 :: f v0 -> f v1
+  slackenApplyX0 = slacken applyX0
+
+  fn :: f v0 -> f v1
+  fn = \fv0 -> slackenApplyX0 fv0
+
+  restrictedFn :: c (f v0) (f v1)
+  restrictedFn = restrict fn
+
+--     slacken (pure' (Proxy3 :: Proxy3 c)) f :: f (c v0 v1)
+--     apply (slacken (pure' (Proxy3 :: Proxy3 c)) f) :: c (f v0) (f v1)
+--     slacken (apply (slacken (pure' (Proxy3 :: Proxy3 c)) f)) fv0 :: f v1
+
+
+-- liftA1 f =
+--   restrict \fx0 -> slacken applyFf fx0
+--   where
+--   ff :: f (c v0 v1)
+-- --   ff = pure' (Proxy3 :: Proxy3 c)
+--   ff = restrict (slacken (pure' (Proxy3 :: Proxy3 c)) f)
+--   applyFf :: c (f v0) (f v1)
+--   applyFf = apply ff
