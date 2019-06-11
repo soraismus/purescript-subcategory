d3b5331f37c94d63d94a2ea6cf6cb0d6e1f9c7d4
matthew.hilty <hilty.matthew@gmail.com>
2019-06-10 20:19:30 -0400

    snapshot

--- src/Endofunctor/Monad.purs
+++ src/Endofunctor/Monad.purs
@@ -2,13 +2,15 @@ module Control.Subcategory.Endofunctor.Monad
   ( class Monad
   ) where
 
+import Prelude (($))
+
 import Control.Subcategory.Constituency (class ObjectOf)
 import Control.Subcategory.Endofunctor.Applicative (class Applicative)
 import Control.Subcategory.Endofunctor.Bind (class Bind)
 import Control.Subcategory.Endofunctor.HasBind (class HasBind, bindFlipped)
 import Control.Subcategory.Endofunctor.HasPure (class HasPure, pure')
 import Control.Subcategory.Restrictable (class Restrictable, restrict)
-import Control.Subcategory.Slackable (class Slackable, slacken)
+import Control.Subcategory.Slackable (class Slackable, slacken, slacken')
 import Type.Proxy (Proxy3(Proxy3))
 
 class (Applicative c m, Bind c m) <= Monad c m
@@ -29,126 +31,61 @@ ap
   => Slackable c
   => m (c v0 v1)
   -> c (m v0) (m v1)
-ap mf = f9
-  where
-  f9 :: c (m v0) (m v1)
-  f9 = restrict f8
-  f8 :: m v0 -> m v1 -- closes over `mf`
-  f8 mv0 = f7 mf
-    where
-    f7 :: m (c v0 v1) -> m v1
-    f7 = slacken f6
-    f6 :: c (m (c v0 v1)) (m v1)
-    f6 = bindFlipped f5
-    f5 :: c (c v0 v1) (m v1)
-    f5 = restrict f4
-    f4 :: c v0 v1 -> m v1 -- closes over `mv0`
-    f4 f = f3 mv0
-      where
-      f3 :: m v0 -> m v1
-      f3 = slacken f2
-      f2 :: c (m v0) (m v1)
-      f2 = bindFlipped f1
-      f1 :: c v0 (m v1)
-      f1 = restrict f0
-      f0 :: v0 -> m v1 -- closes over `f`
-      f0 v0 = x1
-        where
-        fa :: c v1 (m v1)
-        fa = pure' (Proxy3 :: Proxy3 c)
-        fb :: v1 -> m v1
-        fb = slacken fa
+ap mf =
+  restrict \mv0 ->
+      callOn mf
+        $ slacken' (Proxy3 :: Proxy3 c) $ bindFlipped
+        $ restrict \f ->
+              callOn mv0
+                $ slacken' (Proxy3 :: Proxy3 c) $ bindFlipped
+                $ restrict \v0 ->
+                      callOn (slacken f v0)
+                        $ slacken $ pure' (Proxy3 :: Proxy3 c)
 
-        fc :: v0 -> v1
-        fc = slacken f
+-- ap :: forall m a b. Monad m => m (a -> b) -> m a -> m b
+-- ap f a = do
+--   f' <- f
+--   a' <- a
+--   pure (f' a')
 
-        x0 :: v1
-        x0 = fc v0
+callOn :: forall v0 v1. v0 -> (v0 -> v1) -> v1
+callOn x f = f x
 
-        x1 :: m v1
-        x1 = fb x0
-
-
-
---       f0 :: v0 -> m v1 -- closes over `f`
---       f0 v0 = slacken (pure' (Proxy3 :: Proxy3 c)) (f v0)
-
-
--- ap mf = f9
+-- ap mf = g0
 --   where
---   f0 :: v0 -> m v1 -- closes over `f`
---   f0 = \v0 -> slacken (pure' (Proxy3 :: Proxy3 c)) (f v0)
---   f1 :: c v0 (m v1)
---   f1 = restrict f0
---   f2 :: c (m v0) (m v1)
---   f2 = bindFlipped f1
---   f3 :: m v0 -> m v1
---   f3 = slacken f2
---   f4 :: c v0 v1 -> m v1 -- closes over `mv0`
---   f4 = \f -> f3 mv0
---   f5 :: c (c v0 v1) (m v1)
---   f5 = restrict f4
---   f6 :: c (m (c v0 v1)) (m v1)
---   f6 = bindFlipped f5
---   f7 :: m (c v0 v1) -> m v1
---   f7 = slacken f6
---   f8 :: m v0 -> m v1 -- closes over `mf`
---   f8 = \mv0 -> f7 mf
---   f9 :: c (m v0) (m v1)
---   f9 = restrict f8
-
--- ap mf =
---   restrict
---     (\mv0 ->
---       (slacken
---         (bindFlipped
---           (restrict
---             (\f ->
---                 (slacken
---                   (bindFlipped
---                     (restrict (\v0 -> slacken (pure' (Proxy3 :: Proxy3 c)) (f v0)))
---                   )
---                   mv0
---                 )
---             )
---           )
---         )
---         mf
---       )
---     )
-
--- ap mf mv0 = do
---   f' <- mf
---   v0' <- mv0
---   pure (f' v0')
---
--- ap mf mv0 =
---   mf >>= \f' -> mv0 >>= \v0' -> pure (f' v0')
---
--- ap mf mv0 =
---   bindFlipped
---     (\f' -> mv0 >>= \v0' -> pure (f' v0'))
---     mf
---
--- ap mf mv0 =
---   bindFlipped
---     (\f' ->
---               bindFlipped
---                 (\v0' -> pure (f' v0'))
---                 mv0
---     )
---     mf
---
--- ap f =
---     bindFlipped x1
---   where
---   c = Proxy3 :: Proxy3 c
---   x0 :: c v0 v1 -> v0 -> m v1
---   x0 = \f' -> \v0' -> slacken (pure' c) (slacken f' v0)
--- --   x1 :: c v0 v1 -> c (m v0) (m v1)
--- --   x1 = \f' -> bindFlipped (restrict (x0 f'))
---   x2 :: c v0 v1 -> m v0 -> m v1
---   x2 = \f' -> \mv0' -> slacken (bindFlipped (restrict (x0 f'))) mv0'
+--   g0 :: c (m v0) (m v1)
+--   g0 = restrict g1
+--   g1 :: m v0 -> m v1 -- closes over `mf`
+--   g1 mv0 = g2 mf
+--     where
+--     g2 :: m (c v0 v1) -> m v1
+--     g2 = slacken g3
+--     g3 :: c (m (c v0 v1)) (m v1)
+--     g3 = bindFlipped g4
+--     g4 :: c (c v0 v1) (m v1)
+--     g4 = restrict g5
+--     g5 :: c v0 v1 -> m v1 -- closes over `mv0`
+--     g5 f = g6 mv0
+--       where
+--       g6 :: m v0 -> m v1
+--       g6 = slacken g7
+--       g7 :: c (m v0) (m v1)
+--       g7 = bindFlipped g8
+--       g8 :: c v0 (m v1)
+--       g8 = restrict g9
+--       g9 :: v0 -> m v1 -- closes over `f`
+--       g9 v0 = x1
+--         where
+--         g10 :: c v1 (m v1)
+--         g10 = pure' (Proxy3 :: Proxy3 c)
+--         g11 :: v1 -> m v1
+--         g11 = slacken g10
+--         g12 :: v0 -> v1
+--         g12 = slacken f
+--         x0 :: v1
+--         x0 = g12 v0
+--         x1 :: m v1
+--         x1 = g11 x0
 
 
 -- liftM1 :: forall m v0 v1. Monad m => (v0 -> v1) -> m v0 -> m v1
--- src/Slackable.purs
+++ src/Slackable.purs
@@ -1,28 +1,43 @@
 module Control.Subcategory.Slackable
   ( class Slackable
   , slacken
+  , slacken'
   ) where
 
 import Control.Subcategory.Constituency (class ObjectOf)
 import Data.Function (apply) as Function
 import Record.Builder (Builder)
 import Record.Builder (build) as Builder
+import Type.Proxy (Proxy3(Proxy3))
 import Unsafe.Coerce (unsafeCoerce)
 
 class Slackable (c :: Type -> Type -> Type) where
-  slacken
+  slacken'
     :: forall v0 v1
      . ObjectOf c v0
     => ObjectOf c v1
-    => (c v0 v1)
+    => Proxy3 c
+    -> c v0 v1
     -> v0
     -> v1
 
+slacken
+  :: forall c v0 v1
+   . ObjectOf c v0
+  => ObjectOf c v1
+  => Slackable c
+  => c v0 v1
+  -> v0
+  -> v1
+slacken = slacken' (Proxy3 :: Proxy3 c)
+
+
+
 instance slackableFn :: Slackable Function where
-  slacken = Function.apply
+  slacken' _ = Function.apply
 
 instance slackableBuilder :: Slackable Builder where
-  slacken builder record = coerceBuild Builder.build builder record
+  slacken' _ builder record = coerceBuild Builder.build builder record
     where
     coerceBuild
       :: (forall r1 r2
