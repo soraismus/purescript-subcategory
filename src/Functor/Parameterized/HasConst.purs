a2e2196f134f5a0a9a8746ac9a4a64fbe901f693
matthew.hilty <hilty.matthew@gmail.com>
2019-06-15 18:00:03 -0400

    snapshot

--- src/Functor/Parameterized/HasConst.purs
+++ src/Functor/Parameterized/HasConst.purs
@@ -1,12 +1,11 @@
 module Control.Subcategory.Functor.Parameterized.HasConst
   ( class HasConst
   , const
-  , const'
   ) where
 
 import Control.Subcategory.Constituency (class ObjectOf)
 import Control.Subcategory.Functor.HasPure (class HasPure, pure')
-import Control.Subcategory.Restrictable (class Restrictable, restrict)
+import Control.Subcategory.Restrictable (class Restrictable)
 import Type.Proxy (Proxy3(Proxy3))
 
 class HasConst
@@ -19,12 +18,6 @@ class HasConst
     => ObjectOf c v'
     => v'
     -> c v v'
-  const'
-    :: forall v'
-     . ObjectOf c v
-    => ObjectOf c v'
-    => ObjectOf c (c v v')
-    => c v' (c v v')
 
 instance constRestrictableFixedSourceArrow
   :: ( HasPure c (c v)
@@ -33,4 +26,3 @@ instance constRestrictableFixedSourceArrow
   => HasConst c v
   where
   const x = pure' (Proxy3 :: Proxy3 c) x
-  const' = restrict \x -> pure' (Proxy3 :: Proxy3 c) x
